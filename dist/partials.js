d1327839e98e76f8eb96a7e42156e99f01f1cb33
Cory Brown <oh.wise.man@gmail.com>
2014-10-29 08:16:29 -0600

    first commit

---
+++ dist/partials.js
@@ -0,0 +1,118 @@
+this["Handlebars"] = this["Handlebars"] || {};
+this["Handlebars"]["Templates"] = this["Handlebars"]["Templates"] || {};
+
+Handlebars.registerPartial("end", Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "\n<section class=\"section-title\" data-background=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.theme)),stack1 == null || stack1 === false ? stack1 : stack1.mainColor)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\" data-transition=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.theme)),stack1 == null || stack1 === false ? stack1 : stack1.specialTransition)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">\n	<h2>";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h2>\n	<h3>";
+  if (helper = helpers.subtitle) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.subtitle); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h3>\n</section>\n";
+  return buffer;
+  }));
+
+Handlebars.registerPartial("section", Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1, helper;
+  buffer += "<h3>";
+  if (helper = helpers.subtitle) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.subtitle); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h3>";
+  return buffer;
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = "", stack1, helper;
+  buffer += "\n		<section>\n			<h2>";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h2>\n			";
+  if (helper = helpers.uniqueContent) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.uniqueContent); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n		</section>\n	";
+  return buffer;
+  }
+
+  buffer += "\n<section class=\"section-title\" data-background=\"#fead4c\" data-transition=\"zoom\">\n	<h1>";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h1>\n	";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.subtitle), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</section>\n\n	\n	";
+  stack1 = helpers.each.call(depth0, (depth0 && depth0.slides), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  }));
+
+Handlebars.registerPartial("start", Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1, helper;
+  buffer += "\n			<li class=\"fragment\">";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</li>\n		";
+  return buffer;
+  }
+
+  buffer += "\n<section class=\"section-title\" data-background=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.theme)),stack1 == null || stack1 === false ? stack1 : stack1.mainColor)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\" data-transition=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.theme)),stack1 == null || stack1 === false ? stack1 : stack1.specialTransition)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">\n	<h2>";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h2>\n	<h3>";
+  if (helper = helpers.subtitle) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.subtitle); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h3>\n</section>\n\n\n<section>\n	<img class=\"constrain-width\" src=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.githubData)),stack1 == null || stack1 === false ? stack1 : stack1.avatar_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\" alt=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.githubData)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "'s avatar'\" />\n	<h2>";
+  if (helper = helpers.greetingText) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.greetingText); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "</h2>\n	<p>"
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.githubData)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</p>\n	<p><a href=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.githubData)),stack1 == null || stack1 === false ? stack1 : stack1.html_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">@"
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.githubData)),stack1 == null || stack1 === false ? stack1 : stack1.login)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a>\n</section>\n\n\n<section>\n	<h2>Presentation Overview</h2>\n	<ul>\n		";
+  stack1 = helpers.each.call(depth0, (depth0 && depth0.sections), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n	</ul>\n</section>\n";
+  return buffer;
+  }));
---
+++ src/data.js
@@ -0,0 +1,274 @@
+// ================================================================================
+// DATA
+// ================================================================================
+
+'use strict';
+
+// Grab user's github data via the GitHub API
+var githubData = getData('https://api.github.com/users/uniqname');
+
+// Global context for Handlebars templates in `src/templates`
+var data = {
+	githubData: githubData,
+
+
+
+	// ------------------------------------------
+	// CUSTOMIZE BELOW
+	// ------------------------------------------
+
+	theme: {
+		mainColor: '#fead4c',
+		specialTransition: 'zoom'
+	},
+	title: 'Web Components and Componentized Architecture',
+	subtitle: 'Componets are coming!',
+	greetingText: 'I &#10084; Components',
+	goodbyeText: 'U &#10084; Components 2?',
+	sections: [
+		{
+			title: 'Code modularization',
+			subtitle: 'This is a subtitle',
+			slides: [
+				{
+					title: 'Principles',
+					uniqueContent: '
+						<ul>
+							<li>Single Responsibility Principle (SRP)</li>
+							<li>Detail Abstraction</li>
+							<li>Reuse</li>
+							<li>Composition</li>
+							<li>Syndication</li>
+						</ul>
+					'
+				},
+				{
+					title: 'SRP',
+					uniqueContent: '
+						<p>Provides module cohesion.</p>
+						<pre class="fragment"><code>
+						var makeList = function (items) {
+							var list = items ? items.slice() : [];
+
+							list.addItem = function (item) { ... };
+							list.removeItem = function (itemID) { ... };
+
+							totalNumOfLists++; // Not SRP.
+
+							return list;
+
+						}
+						</code></pre>
+					'
+				},
+				{
+					title: 'Detail Abstraction',
+					uniqueContent: '
+						<p>Black Box Development.</p>
+						<pre class="fragment"><code>
+	var makeList = (function () {
+
+		// These are abstracted implementation details
+		var lastID = 0,
+			idGen = function () {
+				return listID++;
+			},
+
+			listMaker = function (items) {
+				var list = items ? items.slice() : [];
+
+				list.addItem = function (item) { ... };
+				list.removeItem = function (itemID) { ... };
+				list.id = idGen();
+
+				return list;
+			};
+
+		// Only the listMaker function is exposed.
+		return listMaker;
+		})();
+
+	}
+						</code></pre>
+					'
+				},
+				{
+					title: 'Reuse',
+					uniqueContent: '
+						<p>Object Instantiation / Functinoal Development.</p>
+						<pre class="fragment"><code>
+	var makeList = function (items) {
+		var list = items ? items.slice() : [];
+		
+		...
+		//This function will always generate a new object.
+		return list;
+	};
+						</code></pre>
+					'
+				},
+				{
+					title: 'Composition',
+					uniqueContent: '
+						<p>Additive Complexity</p>
+						<pre class="fragment"><code>
+	var makeTask = function (item) { ... },
+
+		//makeList is composed of several makeTask calls.
+		makeList = function (tasks) { 
+			return tasks.map(function (task) {
+				return makeTask(task);
+			});
+		};
+						</code></pre>
+					'
+				},
+				{
+					title: 'Syndication',
+					uniqueContent: '
+						<p>Portability / Consumability</p>
+						<pre class="fragment"><code>
+						var makeTask = function (item) { ... },
+
+							//makeList.js
+							module.exports = function (tasks) { 
+								return tasks.map(function (task) {
+									return makeTask(task);
+								});
+							};
+						</code></pre>
+					'
+				},
+				{
+					title: 'Syndication',
+					uniqueContent: '
+						<p>Portability / Consumability</p>
+						
+						<p>One could make this consumable by anyone at ICS. Simply include a `package.json` and publish</p>
+						<pre class="fragment"><code class="bash">
+						~/my-module/$ npm publish
+						</code></pre>
+					'
+				}
+			]
+		},
+		{
+			title: 'JS Modules',
+			subtitle: '',
+			slides: [
+				{
+					title: 'AMD',
+					uniqueContent: '
+						<p>Needs a library like requirejs to inject moduels</p>
+						<pre class="fragment"><code>
+						//moduleA.js
+
+						define(["dependencyA"] , function (depA) {
+						    return function () {};
+						});
+						</code></pre>
+						<small class="fragment">(Old and busted)</small>
+					'
+				},
+				{
+					title: 'CommonJS',
+					uniqueContent: '
+						<p>Needs a library like Browserify for Front-end components</p>
+						<pre class="fragment"><code>
+						//moduleB.js
+
+						var depB = require("dependencyB");
+
+					    module.exports = function () {
+					     	return depB();
+					    };
+						</code></pre>
+						<small class="fragment">(Native module syntax in Node)</small>
+					'
+				},
+				{
+					title: 'ES6 Modules',
+					uniqueContent: '
+						<p>Needs a transpiler until browser support improves</p>
+						<pre class="fragment"><code>
+						//moduleC.js
+
+						import * as depC from "dependencyC";
+
+						var moduleC = function () {
+							return depC.method();	
+						}
+
+						export function moduleC;
+						</code></pre>
+						<small class="fragment">(Native browser module syntax)</small>
+					'
+				}
+			]
+		},
+		{
+			title: 'Componentized Architecture',
+			subtitle: 'Web Components & web components',
+			slides: [
+				{
+					title: 'Principles of Componentized Architecture',
+					uniqueContent: '
+						<ul>
+							<li class="fragment">Single Responsibility Principle</li>
+							<li class="fragment">Detail Abstraction</li>
+							<li class="fragment">Reuse</li>
+							<li class="fragment">Composition</li>
+							<li class="fragment">Syndication</li>
+						</ul>
+					'
+				}
+			]
+		},
+		{
+			title: 'Web Components',
+			subtitle: 'Big "W", big "C"',
+			slides: [
+				{
+					title: 'Core Technologies',
+					uniqueContent: '
+						<ul class="part-counterpart">
+							<li><span data-fragment-index="1" class="fragment part">Custom Elements</span> <span data-fragment-index="5" class="fragment counterpart">SRP</span></li>
+							<li><span data-fragment-index="2" class="fragment" part>Shadow DOM</span> <span data-fragment-index="6" class="fragment counterpart">Detail Abstraction</span></li>
+							<li><span data-fragment-index="3" class="fragment part">Templates</span> <span data-fragment-index="7" class="fragment counterpart">Reuse</span></li>
+							<li><span data-fragment-index="10" class="fragment part">HTML</span> <span data-fragment-index="8" class="fragment counterpart">Composition</span></li>
+							<li><span data-fragment-index="4" class="fragment part">HTML Imports</span> <span data-fragment-index="9" class="fragment counterpart">Syndication</span></li>
+						</ul>
+					'
+				},
+				{
+					title: 'Can I Haz?!?!',
+					uniqueContent: '
+						<img src="images/canihaz.jpg" alt="Can I Haz Awesum?" />
+					'
+				},
+				{
+					title: 'CanIUse',
+					uniqueContent: '
+						<p><a href="//caniuse.com/#search=web%20components">caniuse.com/#search=web%20components</a></p>
+
+						<iframe src="//caniuse.com/#search=web%20components" frameborder="0"></iframe>
+					'
+				},
+				{
+					title: 'Polyfill',
+					uniqueContent: '
+						<p><a href="https://github.com/Polymer/webcomponentsjs">WebComponents.js</a></p>
+
+						<img src="images/polymer-logo.svg" alt="Polymer" />
+					'
+				}
+			]
+		},
+		{
+			title: 'DEMO TIME',
+			subtitle: 'Accorion Web Component',
+			slides: [
+			]
+		}
+	]
+};
